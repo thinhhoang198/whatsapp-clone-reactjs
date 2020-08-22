b83296ae0d1748be56038f987fa1c893856dfd38
Thinh <ducthinh.hoang198@gmail.com>
2020-08-22 17:43:41 +1000

    commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBj2uFv7TsztISIhYcBogY02R1NPdBuVnE",
+  authDomain: "whatsapp-clone-reactjs-f18fa.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-reactjs-f18fa.firebaseio.com",
+  projectId: "whatsapp-clone-reactjs-f18fa",
+  storageBucket: "whatsapp-clone-reactjs-f18fa.appspot.com",
+  messagingSenderId: "301488273858",
+  appId: "1:301488273858:web:fdded0d427d115a7063e5b",
+  measurementId: "G-C4K5HKDBJ8",
+};
